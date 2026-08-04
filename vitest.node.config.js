9136d67fc570bd4378be195e7cd679a5855e5e17
Aayush Kapoor <aayushkapoor34@gmail.com>
2026-08-04 19:16:18 -0400

    chore: resolve path alias for tests

--- vitest.node.config.js
+++ vitest.node.config.js
@@ -1,6 +1,17 @@
+import { fileURLToPath } from 'node:url';
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: './worker-source.js',
+        replacement: fileURLToPath(
+          new URL('./dist/runtime/worker-source.js', import.meta.url),
+        ),
+      },
+    ],
+  },
   test: {
     env: {
       RUN_CONTINUATION_SECRET: 'run-test-continuation-secret-32-bytes',
